547153336bf4d2eeb48cdcb0cd5cc715553b5dda
tsbowler <tsbowler@bu.edu>
2023-09-19 15:07:57 -0400

    Updated ChatRoom.js so the user can send messages that are logged in the Firestore database and appear on the page

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { collection, query, orderBy } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { collection, addDoc, serverTimestamp, query, orderBy } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-function ChatRoom({ db }) {
+function ChatRoom({ user, db }) {
+  const [message, setMessage] = useState('');
   const messagesRef = collection(db, 'messages');
-  const q = query(messagesRef, orderBy('createdAt', 'desc'));
+  const q = query(messagesRef, orderBy('createdAt', 'asc'));
   const [messages] = useCollectionData(q, { idField: 'id' });
+
+  const sendMessage = async () => {
+    if (message.trim() === '') return;
+
+    try {
+      await addDoc(messagesRef, {
+        text: message,
+        createdAt: serverTimestamp(),
+        user: user.displayName, // Use the user's display name or ID
+      });
+      setMessage('');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
 
   return (
     <div>
-      {messages && messages.length > 0 ? (
-        messages.map((message) => (
-          <div key={message.id}>{message.text}</div>
-        ))
-      ) : (
-        <h1>Looks like you don't have any messages yet...</h1>
-      )}
+      <div className="chat-messages">
+        {messages &&
+          messages.map((msg) => (
+            <div key={msg.id}>
+              <strong>{msg.user}:</strong> {msg.text}
+            </div>
+          ))}
+      </div>
+      <input
+        type="text"
+        placeholder="Type your message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
 }
